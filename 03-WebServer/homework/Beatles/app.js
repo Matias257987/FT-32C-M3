e64e351d12c9f8f89571be96d821d269a8833026
Matias <acostamatias25@gmail.com>
2022-11-22 10:32:18 -0300

    WebServer

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs   = require('fs');
+const { url } = require('inspector');
 
 var beatles=[{
   name: "John Lennon",
@@ -22,3 +23,48 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+
+http.createServer((req, res) => {
+  if (req.url === "/") {
+    let html = fs.readFileSync(__dirname + "/index.html");
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+  }
+  else if (req.url === "/api" || req.url === "/api/") {
+    res.writeHead(200, {"Content-Type" : "application/json"});
+    res.end(JSON.stringify(beatles));
+  } else {
+    if (req.url.substring(0, 5) === "/api/") {
+      let urlName = req.url.substring(5).replace("%20", " ");
+      let beatle = beatles.find(artista => artista.name.toLowerCase() === urlName.toLowerCase());
+
+      if (!beatle) {
+        let html = fs.readFileSync(__dirname + "/404.html");
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end(html);
+      } else {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(beatle));
+      }
+    } else {
+      let name = req.url.substring(1).replace("%20", " ");
+      let beatle = beatles.find(artista => artista.name.toLowerCase() === name.toLowerCase());
+      let beatleHTML = fs.readFileSync(__dirname + "/beatle.html", "utf-8");
+
+      if (!beatle) {
+        let html = fs.readFileSync(__dirname + "/404.html");
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end(html);
+      } else {
+        beatleHTML = beatleHTML
+          .replace("{titulo}", beatle.name)
+          .replace("{nombre}", beatle.name)
+          .replace("{nacimiento}", beatle.birthdate)
+          .replace("{img}", beatle.profilePic);
+          res.writeHead(200, {"Content-Type": "text/html"});
+          res.end(beatleHTML);
+      }
+    }
+  }
+}).listen(3000, console.log("localhost:3000"))
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,20 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+
+http.createServer(function (request, response) {
+    if (request.url === '/') {
+        response.writeHead(200, {'Content-Type':'text/plain'});
+        response.end("Estas en el Home");
+    } else {
+        fs.readFile(__dirname + `/images${request.url}_doge.jpg`, (err, img) => {
+            if (err) {
+                response.writeHead(404);
+                response.end("Imagen no encontrada");
+            } else {
+                response.writeHead(200);
+                response.end(img);
+            }
+        })
+    }
+}).listen(3000, console.log("Server running localhost:3000"))
